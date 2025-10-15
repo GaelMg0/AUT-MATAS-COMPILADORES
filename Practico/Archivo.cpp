9f6440f20df7270868298f2016703ba66cc41761
GaelMarquez <ma427833@uaeh.edu.mx>
2025-10-15 10:52:23 -0600

    Add files via upload

---
+++ Practico/Archivo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Comentario simple
+    /* Comentario
+       multilínea */
+    int x = 5;
+    float y = 3.14;
+    if (x > 0 && y != 0) {
+        x++;
+    }
+    return 0;
+}
